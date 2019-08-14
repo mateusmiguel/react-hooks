0fae586d9371ed4205f5d5fce4b94072c406e850
Mateus Miguel <webmateusmiguel@gmail.com>
2019-08-14 20:18:38 -0300

    Hooks

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import React, { useState, useEffect } from "react";
 export default function App() {
   const [repositories, setRepositories] = useState([]);
 
-  useEffect(async () => {
-    const response = await fetch(
-      "https://api.github.com/users/mateusmiguel/repos"
-    );
-    const data = await response.json();
-
-    setRepositories(data);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://api.github.com/users/mateusmiguel/repos"
+      );
+      const data = await response.json();
+      setRepositories(data);
+    }
+    fetchData();
   }, []);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: true } : { repo };
+    });
+    setRepositories(newRepositories);
+  }
 
   return (
     <ul>
       {repositories.map(repo => (
-        <li key={repo.id}>{repo.name}</li>
+        <li key={repo.id}>
+          {repo.name}
+          {repo.favorite && <span>Favorito</span>}
+          <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+        </li>
       ))}
     </ul>
   );
